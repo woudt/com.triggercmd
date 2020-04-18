24f3068619e7525a5c2a09b3298a1b8389aab9c8
Edwin Woudt <edwin@woudt.nl>
2020-04-18 22:40:53 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+TRIGGERcmd is a cloud service that allows you to securely and remotely run commands on your computers.
+
+Usage instructions:
+- Register on https://www.triggercmd.com
+- Install an agent on a windows, linux or mac computer
+- Install this app and copy/paste the token from the TRIGGERcmd instructions page (available after login) in the configuration page of this app
---
+++ app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Homey = require('homey');
+const fetch = require('node-fetch');
+
+class TriggerCmdApp extends Homey.App {
+	
+	onInit() {
+		this.log('TriggerCmdApp is running...');
+
+		let myAction = new Homey.FlowCardAction('run-command');
+		myAction.register().registerRunListener(async ( args, state ) => {
+			try {
+				const body = "trigger=" + args.command.did;
+				this.log(body);
+				const token = Homey.ManagerSettings.get('token');
+				const response = await fetch(
+					"https://www.triggercmd.com/api/run/triggerById", {
+					method: "POST",	
+					headers: {
+						"Authorization": "Bearer " + token,
+						"Content-Type": "application/x-www-form-urlencoded"
+					},
+					body: body
+				});
+				const result = await response.text();
+				this.log(result);
+				if (result.includes("err")) {
+					throw new Error(result);
+				};
+			} catch (error) {
+				this.log(error);
+				throw error;
+			}
+		});
+    	myAction.getArgument('command').registerAutocompleteListener(async ( query, args ) => {
+			try {
+				const token = Homey.ManagerSettings.get('token');
+				const response = await fetch(
+					"https://www.triggercmd.com/api/command/commandlist", {
+					headers: {
+						"Authorization": "Bearer " + token
+					}
+				});
+				const result = await response.json();
+				this.log(result)
+				return result
+			} catch (error) {
+				this.log(error)
+				throw error;
+			}
+		});
+	}
+	
+	async _onFlowCommandAutocomplete( query ) {
+		return await fetch("https://www.triggercmd.com/api/command/commandlist", {headers: {
+			"authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOTg2Zjg2ZDJkYmE1MDAxOTVlZDYwZSIsImlhdCI6MTU4NzIyNTA3N30.6YsjNY8z2Nn-VUrUZGN1FRGAYPMTWfqZZMFewZcC4WU"
+		}}).json()
+	}
+}
+
+module.exports = TriggerCmdApp;
